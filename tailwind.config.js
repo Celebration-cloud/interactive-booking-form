abc8e8941a422a64ca12590933936628b2b2377d
Celebration-cloud <ojinguluc@gmail.com>
2025-02-06 01:25:11 +0100

    spinner

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,7 @@ module.exports = {
   content: [
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./node_modules/@heroui/theme/dist/components/(button|card|checkbox|code|date-picker|input|kbd|link|listbox|navbar|slider|snippet|toggle|table|ripple|spinner|form|calendar|date-input|popover|divider|spacer).js",
+    "./node_modules/@heroui/theme/dist/components/(button|card|checkbox|code|date-picker|input|kbd|link|listbox|navbar|slider|snippet|spinner|toggle|table|ripple|form|calendar|date-input|popover|divider|spacer).js",
   ],
   theme: {
     extend: {
